ef9ac8d24b90b901504502587a859895e7ff8e43
donydony228 <donydony228@gmail.com>
2025-03-14 14:17:37 +0800

    fix: spec issue

--- app/router/record.py
+++ app/router/record.py
@@ -10,33 +10,33 @@ from .. import schemas
 router = APIRouter()
 
 # **取得所有地點**
-@router.get("/records", response_model=dict)
-def get_all_records(db: Session = Depends(get_db)):
-    record_list = Record.get_records(db)
+# @router.get("/records", response_model=dict)
+# def get_all_records(db: Session = Depends(get_db)):
+#     record_list = Record.get_records(db)
 
-    if not record_list:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="沒有找到任何地點資料"
-        )
+#     if not record_list:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail="沒有找到任何地點資料"
+#         )
 
-    # 這裡的資料格式名是對應　models.py　的 Location
-    return {
-    "status": "success",
-    "data": [
-        schemas.RecordResponse(
-            recordid=rec.RecordID,  # 保留大寫
-            semester=rec.Semester,  # 小寫
-            date=rec.Date,  # ORM 是大寫，但 Schema 需要小寫
-            photo=rec.Photo,
-            description=rec.Description,
-            location=rec.Location,
-            # villager=rec.Villager,
-            account=rec.Account,
-        )
-        for rec in record_list
-    ]
-}
+#     # 這裡的資料格式名是對應　models.py　的 Location
+#     return {
+#     "status": "success",
+#     "data": [
+#         schemas.RecordResponse(
+#             recordid=rec.RecordID,  # 保留大寫
+#             semester=rec.Semester,  # 小寫
+#             date=rec.Date,  # ORM 是大寫，但 Schema 需要小寫
+#             photo=rec.Photo,
+#             description=rec.Description,
+#             location=rec.Location,
+#             # villager=rec.Villager,
+#             account=rec.Account,
+#         )
+#         for rec in record_list
+#     ]
+# }
 
 # **取得特定地點的家訪紀錄**
 @router.post("/records", response_model=dict)
--- app/schemas.py
+++ app/schemas.py
@@ -19,6 +19,7 @@ class LocationResponse(BaseModel):
     latitude: str  # 對應 ORM 的 `Latitude`
     longitude: str  # 對應 ORM 的 `Longitude`
     # address: Optional[str]=None  # `Address` 可能是 NULL，所以加 `None`
+    brief_description: Optional[str]=None  # `BriefDescription` 可能是 NULL，所以加 `None`
 
     class Config:
         from_attributes = True  # 允許 SQLAlchemy ORM 自動轉換為 Pydantic 模型
